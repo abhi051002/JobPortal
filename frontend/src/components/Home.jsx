42600dd96bc3a867fe4bf6b697e2c59dda1a5825
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-11-16 20:55:05 +0530

    Herosection & carousel of home done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "./home/HeroSection";
+import CategoryCarousel from "./home/CategoryCarousel";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <HeroSection />
+      <CategoryCarousel />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
