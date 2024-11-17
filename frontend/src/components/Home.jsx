c5b3d068880cacba11d3596a12b6f3f954682038
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-11-17 20:48:25 +0530

    Home Page done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./home/HeroSection";
 import CategoryCarousel from "./home/CategoryCarousel";
+import LatestJobs from "./home/LatestJobs";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <CategoryCarousel />
+      <LatestJobs />
     </div>
   );
 };
