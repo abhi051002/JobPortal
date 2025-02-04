0285f72912b82c43f678d0d2b2a15ff976bafe08
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2025-02-04 22:18:08 +0530

    updated

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,11 +1,11 @@
 export const USER_API_ENDPOINT =
-  "https://job-portal-backend-7t816ik2s-abhi051002s-projects.vercel.app/api/v1/user";
+  "https://job-portal-q7vfffrlx-abhi051002s-projects.vercel.app/api/v1/user";
 export const JOB_API_ENDPOINT =
-  "https://job-portal-backend-7t816ik2s-abhi051002s-projects.vercel.app/api/v1/job";
+  "https://job-portal-q7vfffrlx-abhi051002s-projects.vercel.app/api/v1/job";
 export const APPLICATION_API_ENDPOINT =
-  "https://job-portal-backend-7t816ik2s-abhi051002s-projects.vercel.app/api/v1/application";
+  "https://job-portal-q7vfffrlx-abhi051002s-projects.vercel.app/api/v1/application";
 export const COMPANY_API_ENDPOINT =
-  "https://job-portal-backend-7t816ik2s-abhi051002s-projects.vercel.app/api/v1/company";
+  "https://job-portal-q7vfffrlx-abhi051002s-projects.vercel.app/api/v1/company";
 // export const USER_API_ENDPOINT = "http://localhost:4000/api/v1/user";
 // export const JOB_API_ENDPOINT = "http://localhost:4000/api/v1/job";
 // export const APPLICATION_API_ENDPOINT =
