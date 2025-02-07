48b34a854587a056b3b652f3c8cdeb6951e118f1
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2025-02-07 13:26:55 +0530

    updated

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,13 +1,13 @@
-// export const USER_API_ENDPOINT =
-//   "https://job-portal-rho-rosy.vercel.app/api/v1/user";
-// export const JOB_API_ENDPOINT =
-//   "https://job-portal-rho-rosy.vercel.app/api/v1/job";
-// export const APPLICATION_API_ENDPOINT =
-//   "https://job-portal-rho-rosy.vercel.app/api/v1/application";
-// export const COMPANY_API_ENDPOINT =
-//   "https://job-portal-rho-rosy.vercel.app/api/v1/company";
-export const USER_API_ENDPOINT = "http://localhost:4000/api/v1/user";
-export const JOB_API_ENDPOINT = "http://localhost:4000/api/v1/job";
+export const USER_API_ENDPOINT =
+  "https://job-portal-rho-rosy.vercel.app/api/v1/user";
+export const JOB_API_ENDPOINT =
+  "https://job-portal-rho-rosy.vercel.app/api/v1/job";
 export const APPLICATION_API_ENDPOINT =
-  "http://localhost:4000/api/v1/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:4000/api/v1/company";
+  "https://job-portal-rho-rosy.vercel.app/api/v1/application";
+export const COMPANY_API_ENDPOINT =
+  "https://job-portal-rho-rosy.vercel.app/api/v1/company";
+// export const USER_API_ENDPOINT = "http://localhost:4000/api/v1/user";
+// export const JOB_API_ENDPOINT = "http://localhost:4000/api/v1/job";
+// export const APPLICATION_API_ENDPOINT =
+//   "http://localhost:4000/api/v1/application";
+// export const COMPANY_API_ENDPOINT = "http://localhost:4000/api/v1/company";
